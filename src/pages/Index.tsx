5a8580fc0e6c0c6b04e328e04544e0fe703ecbbb
Phanct02 <taiphanchi19@gmail.com>
2025-10-28 21:31:29 +0700

    Initial commit

---
+++ src/pages/Index.tsx
@@ -0,0 +1,27 @@
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import Topics from "@/components/Topics";
+import Tips from "@/components/Tips";
+import SecurityTools from "@/components/SecurityTools";
+import Resources from "@/components/Resources";
+import FAQ from "@/components/FAQ";
+import Footer from "@/components/Footer";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen">
+      <Header />
+      <main>
+        <Hero />
+        <Topics />
+        <Tips />
+        <SecurityTools />
+        <Resources />
+        <FAQ />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
